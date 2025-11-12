ce125e78f68f1bc6ffe7206805b505dd0b56f41f
Anderson <97542626+Rosier-Anderson@users.noreply.github.com>
2025-11-12 07:36:57 +0000

    Creating Auth  page + Providers

---
+++ app/(auth)/login/page.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Login() {
+  return (
+    <div id="box" className="flex justify-center size-screen text-lg">
+      <div className="">
+        <div className="">
+          <h1 className="font-bold">Sign in</h1>
+          <p className="">Enter your account to sign in </p>
+        </div>
+        <div className=""></div>
+      </div>
+    </div>
+  );
+}
+
+export default Login;
